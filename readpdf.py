e38bd9d9a92386ebd12e746b5d8a5cf80b1401b3
Adrien Jaillet <adrien.jaillet@insa-lyon.fr>
2020-02-19 23:37:59 +0100

    ParseFactureProduit

--- readpdf.py
+++ readpdf.py
@@ -22,47 +22,139 @@ f = open('./Hello.txt','r')
 extracts = []
 i=0
 
-#On trie que les pages qui nous semblent intéressantes
 for page in f.readlines():
-    extracts.append(page.split('CLIENT FACTURE',1))
-    if(len(extracts[i])>1):
-        del extracts[i][1]
+    extracts.append(page)
+
+#On crée 2 listes pour séparer les factures des avoirs
+i=0
+j=0
+listfactures = []
+for page in extracts:
+    if 'FACTURECOMMUNAUTE' in page:
+        listfactures.append(page)
+
+listavoir = []
+for page in extracts:
+    if 'AVOIRCOMMUNAUTE' in page:
+        listavoir.append(page)
+
+
+#travail sur les factures :
+
+asupprimer = []
+#suppression des factures vides
+for i in range (0,len(listfactures)):
+    pageparser = listfactures[i].split(" ")
+    if pageparser[0] == 'Commande':
+        asupprimer.append(i)
+
+for i in range(len(asupprimer)-1,-1,-1):
+    del listfactures[asupprimer[i]]
+
+#ici on parse le document pour enlever les parties qui ne nous intéressent pas
+asupprimer = []
+listfactures2 = []
+for page in listfactures:
+    listfactures2.append(page.split('CLIENT FACTURE',1)[0])
+for i in range(0,len(listfactures)):
+    listfactures[i] = listfactures2[i]
+listfactures2 = []
+for page in listfactures:
+    listfactures2.append(page.split('Total Déconsigne',1)[0])
+for i in range(0,len(listfactures)):
+    listfactures[i] = listfactures2[i]
+listfactures2 = []
+for page in listfactures:
+    if page[0]!='_':
+        listfactures2.append([listfactures.index(page),page.split('_',1)[0]])
+for i in range(0,len(listfactures2)):
+    listfactures[listfactures2[i][0]] = listfactures2[i][1]
+
+#On va encore décomposer pour se faciliter la tache :
+#On sépare les articles des consignes
+
+listarticle = []
+listconsigne = []
+
+
+for page in listfactures:
+    if page[0]=='_':
+        listconsigne.append(page)
     else:
-        del extracts[i]
-        i=i-1
+        listarticle.append(page)
 
-    extracts[i][0]=extracts[i][0].replace('_', '')
-    extracts[i][0]=extracts[i][0].replace('|', '')
-    i+=1
+listarticleparse = []
+for i in range(0,len(listarticle)):
+    parse=listarticle[i].split(' ')
+    #Pour parser joliment
+    while '' in parse:
+        del parse[parse.index('')]
 
-# Ici, la liste extract contient beaucoup d'éléments,
-# Je vais créer une autre liste pour gérer seulement les article
+    #On enleve les elements à virgules qui servent un peut a rien (aussi ceux des °)
+    asupprimer = []
+    asup = []
+    for j in range (0,len(parse)):
+        if ',' in parse[j]:
+            asupprimer.append(j)
+        if '°' in parse[j]:
+            asupprimer.append(j)
 
-listArticle = extracts
-listAsuppr = []
-for page in listArticle:
-    parser = page[0].split(' ')
-    if parser[0] in ['Montant','Commande','chèque','Solde','3TVA']:
-        if page not in listAsuppr:
-            listAsuppr.append(listArticle.index(page))
-    elif 'livraison' in page[0]:
-        if page not in listAsuppr:
-            listAsuppr.append(listArticle.index(page))
-    elif 'RECLAMATION' in page[0]:
-        if page not in listAsuppr:
-            listAsuppr.append(listArticle.index(page))        
+    for j in range(0,len(asupprimer)-1):  #On remet la grolsch
+        if '19,5L' == parse[asupprimer[j]]:
+            asup.append(j)
+    for j in range(len(asup)-1,-1,-1):
+        del asupprimer[asup[j]]
 
-for index in range(0,len(listAsuppr)):
-    del listArticle[listAsuppr[index]]
-    for index2 in range(0,len(listAsuppr)):
-        listAsuppr[index2] = listAsuppr[index2]-1
+    for j in range(len(asupprimer)-1,-1,-1):
+        del parse[asupprimer[j]]
+    del parse[0]
+    parse = (' '.join(parse)).split('net')
+    if len(parse) >1:
+        listarticleparse.append(parse)
+#print(listarticleparse)
 
-print(len(listArticle))
-for page in listArticle:
-    print(page)
-    print('\r\n')
+#On remet en place le nombre de colis et cols
+for i in range (0,len(listarticleparse)):
+    for j in range(0,len(listarticleparse[i])):
+        parse = listarticleparse[i][j].split()
+        if len(parse)>0:
+            parseder = list(parse[len(parse)-1])
+            if(len(parseder)<5):
+                der = parseder[0]+' '+''.join(parseder[1:len(parseder)])
+            else:
+                der = ''.join(parseder[0:2])+' '+''.join(parseder[2:len(parseder)])
+            parse[len(parse)-1]=der
+        listarticleparse[i][j]=' '.join(parse)
 
-f.close()
-  
+#On enleve toutes les infos inutiles pour avoir ce qu'on veut traiter
+listarticle = []
+for i in range (0,len(listarticleparse)):
+    for j in range(0,len(listarticleparse[i])):
+        articleparse = listarticleparse[i][j].split()
+        if len(articleparse) > 3:
+            del articleparse[len(articleparse)-1]
+            del articleparse[len(articleparse)-2]
+        article = ' '.join(articleparse)
+        if ' FUT ' in article:
+            article = article.split(' FUT ',1)
+        elif ' VC ' in article:
+            article = article.split(' VC ',1)
+        elif ' VP ' in article:
+            article = article.split(' VP ',1)
+
+        listarticle.append(article)
+
+
+# for i in range (0,len(listarticle)-1):
+#     article= []
+#     if len(listarticle[i])>2:
+#         articleparse = listarticle[i].split
+#         article.append(' '.join(articleparse[0:len(articleparse)-1]))
+#         article.append(articleparse[len(articleparse)-1])
+#         listarticle[i] = article
+
+
+# print(listarticleparse[0])
+        
 # closing the pdf file object 
 pdfFileObj.close() 
