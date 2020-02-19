fbf119fe33fa401435651289cc79012a359f4771
Adrien Jaillet <adrien.jaillet@insa-lyon.fr>
2020-02-19 15:44:25 +0100

    TriDesPagesArticles

--- readpdf.py
+++ readpdf.py
@@ -22,6 +22,7 @@ f = open('./Hello.txt','r')
 extracts = []
 i=0
 
+#On trie que les pages qui nous semblent intéressantes
 for page in f.readlines():
     extracts.append(page.split('CLIENT FACTURE',1))
     if(len(extracts[i])>1):
@@ -29,10 +30,35 @@ for page in f.readlines():
     else:
         del extracts[i]
         i=i-1
-    i+=1
-    
 
-for page in extracts:
+    extracts[i][0]=extracts[i][0].replace('_', '')
+    extracts[i][0]=extracts[i][0].replace('|', '')
+    i+=1
+
+# Ici, la liste extract contient beaucoup d'éléments,
+# Je vais créer une autre liste pour gérer seulement les article
+
+listArticle = extracts
+listAsuppr = []
+for page in listArticle:
+    parser = page[0].split(' ')
+    if parser[0] in ['Montant','Commande','chèque','Solde','3TVA']:
+        if page not in listAsuppr:
+            listAsuppr.append(listArticle.index(page))
+    elif 'livraison' in page[0]:
+        if page not in listAsuppr:
+            listAsuppr.append(listArticle.index(page))
+    elif 'RECLAMATION' in page[0]:
+        if page not in listAsuppr:
+            listAsuppr.append(listArticle.index(page))        
+
+for index in range(0,len(listAsuppr)):
+    del listArticle[listAsuppr[index]]
+    for index2 in range(0,len(listAsuppr)):
+        listAsuppr[index2] = listAsuppr[index2]-1
+
+print(len(listArticle))
+for page in listArticle:
     print(page)
     print('\r\n')
 
